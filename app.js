4bb09fe1d228b1e53def8b89c856b094a3a4b6e1
kong142857 <161849314+kong142857@users.noreply.github.com>
2024-03-02 00:36:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -26,36 +26,41 @@ const html = `
       }, 500);
     </script>
     <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
+        body {
+            color: white;
+            font-family: 'Times New Roman', Times, serif ;
+            font-size: 14px;
+            text-align: center;
+        }
+        .hinh-nen{
+            background-image: url("12345.jpg");
+            background-attachment: fixed;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0.5);
+            background-size:100%;
+            background-position: center;
+            padding: 450px 0 500px;
+            width: 100%;
+        }
+        .box {
+        background-color: orangered;
+        color: white;
+        line-height: 50px;
+        height: 50px;
+        width: 250px;
+        border: dark 2px cidge;
+        border-radius: 5px;
+        }
+        .box:hover {
+        opacity: 0,8;
+        }
     </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
+    <body>
+        <div class="hinh-nen">
+            <br>
+            <h2> Don't miss this special offer </h2>
+            <h3> Get it now for just $10! the price will be increased after 50 downloads </h3>
+            <button type="submit" class="box"> Get the late version of Bootlander </button>       
+        </div>
+    </body>
 </html>
 `
